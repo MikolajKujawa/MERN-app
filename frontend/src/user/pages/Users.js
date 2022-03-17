5b2f059baa0b656e699332050b25a2ec4f671b99
Mikolaj Kujawa <mikolaj.kujawa@erli.pl>
2022-03-17 23:46:17 +0100

    Adding UserList and UserItem components

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return <h2>Users page</h2>
+    const USERS = [{
+        id: 'u1',
+        name: 'Mikołaj Kujawa',
+        image: 'https://filing.pl/wp-content/uploads/2014/06/filing_images_ddad50a54790.png',
+        places: 3
+    }]
+
+    return <UsersList items={USERS}/>
 };
 
 export default Users;
