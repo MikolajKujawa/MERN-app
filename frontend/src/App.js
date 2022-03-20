1816a18eb6e17af16392e29a6bda1f259eef899f
Mikolaj Kujawa <mikolaj.kujawa@erli.pl>
2022-03-20 15:38:10 +0100

    Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,25 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-d
 
 import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-  return <Router>
-    <Switch>
-      <Route path="/" exact>
-        <Users/>
-      </Route>
-      <Route path="/places/new" exact>
-        <NewPlace/>
-      </Route>
-      <Redirect to="/" />
-    </Switch>    
-  </Router>
+  return (
+    <Router>
+      <MainNavigation/>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users/>
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace/>
+          </Route>
+          <Redirect to="/" />
+        </Switch> 
+      </main>         
+    </Router>
+  )
 }
 
 export default App;
