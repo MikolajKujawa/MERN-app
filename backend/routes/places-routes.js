7a2a8243ead16f8f4819923afbe7615f3a4569d0
Mikolaj Kujawa <mikolaj.kujawa@erli.pl>
2022-04-14 23:50:47 +0200

    Handling get methods and errors

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error')
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -18,10 +20,30 @@ const DUMMY_PLACES = [
 
 router.get('/:placeId', (req,res,next) => {
     const placeId = req.params.placeId;
+
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     });
+
+    if(!place) {
+        throw new HttpError('Could not find a place for the provided id.', 404);
+    }
+
     res.json({place})
 });
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    });
+
+    if(!place) {
+        return next(new HttpError('Could not find a place for the provided id.', 404));
+    }
+
+    res.json({place})
+})
 
 module.exports = router;
